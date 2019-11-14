fc979fa229573e991359d734eb4d0ad990fe2136
Coby Tamayo <ctamayo@sitecrafting.com>
2019-11-14 15:23:53 -0800

    implement high-level /search endpoint API

---
+++ src/Client.php
@@ -0,0 +1,182 @@
+<?php
+
+/**
+ * GearLab\Api\Client class
+ *
+ * @copyright 2019 SiteCrafting, Inc.
+ * @author    Coby Tamayo <ctamayo@sitecrafting.com>
+ */
+
+namespace GearLab\Api;
+
+use GuzzleHttp\Client as GuzzleClient;
+use Swagger\Client\Api;
+use Swagger\Client\Configuration;
+
+/**
+ * Central API client for the GearLab Tools REST service
+ */
+class Client {
+  /**
+   * Constructor. Expects an array with configuration data, inluded the
+   * GearLab Tools API key and collection number.
+   *
+   * @param array $opts an associative array with some combination of the
+   * following keys:
+   *
+   * - "key" (string) Your API key as provided or configured in GearLab Tools.
+   *    Required.
+   * - "collection" (int) Your website's Collection ID as provided in GearLab
+   *   Tools. Required.
+   * - "baseUri" (string) The base URI of the REST service to query. Required.
+   */
+  public function __construct(array $opts) {
+    foreach (['key', 'baseUri', 'collection'] as $k) {
+      if (empty($opts[$k])) {
+        throw new \InvalidArgumentException(sprintf(
+          '%s is required',
+          $k
+        ));
+      }
+    }
+
+    $this->key        = $opts['key'];
+    $this->baseUri    = $opts['baseUri'];
+    $this->collection = $opts['collection'];
+
+    $config = $opts['swaggerConfig'] ?? new Configuration();
+    $config->setHost($this->baseUri);
+
+    $this->swaggerClient = $opts['swaggerClient'] ?? new Api\SearchApi(
+      new GuzzleClient(),
+      $config
+    );
+  }
+
+  /**
+   * Perform a search request
+   *
+   * @param array $opts an associative array with a combination of query
+   * parameters. Keys are named exactly as they are in the REST API
+   * docs. `key` and `collection` are passed via the constructor. `query` is
+   * required; all others are optional.
+   * @return array an array like:
+   * ```
+   * array (
+   *  'results' =>
+   *  array (
+   *    0 =>
+   *    array (
+   *      'url' => 'https://bouldercolorado.gov/civic-area/trees-in-the-civic-area-2',
+   *      'title' => 'Trees in the Civic Area',
+   *      'snippet' => 'Updates on plans for trees in the Boulder Civic Area.',
+   *      'meta' =>
+   *      array (
+   *      ),
+   *    ),
+   *    1 =>
+   *    array (
+   *      'url' => 'https://bouldercolorado.gov/forestry/best-trees',
+   *      'title' => 'Best Trees for Boulder',
+   *      'snippet' => 'Select the right tree for the right place.',
+   *      'meta' =>
+   *      array (
+   *      ),
+   *    ),
+   *    // ...more results here...
+   *  ),
+   *  'recommendations' =>
+   *  array (
+   *    0 =>
+   *    array (
+   *      'url' => 'https://bouldercolorado.gov/open-data/city-of-boulder-public-trees/',
+   *      'title' => 'Public Trees',
+   *      'snippet' => 'Public Trees managed by the Urban Forestry Division of the Department of Parks and Recreation, City of Boulder. The provided tree data does not include trees on Open Space and Mountain Parks (OSMP) land. City ordinances prohibit the picking of fruit on trees located on OSMP property. For a list of u',
+   *      'meta' => NULL,
+   *    ),
+   *  ),
+   *  'resOffset' => 0,
+   *  'resCount' => 10,
+   *  'resStart' => 1,
+   *  'resEnd' => 10,
+   *  'total' => 110,
+   *  'literalQuery' => false,
+   *  'originalQueryPhrase' => 'trees',
+   *  'suggestionSupersededQuery' => false,
+   *  'supersedingSuggestion' => '',
+   * )
+   * ```
+   */
+  public function search(array $opts) : array {
+    $response = call_user_func_array(
+      [$this->swaggerClient, 'search'],
+      $this->getSearchParams($opts)
+    );
+
+    // drill down into the cruft
+    $resultSet = $response->getResSet()[0] ?? null;
+
+    if (empty($resultSet)) {
+      return [];
+    }
+
+    $wrappedResults         = $resultSet->getResults();
+    $wrappedRecommendations = $resultSet->getRecommendations();
+    // NOTE: we are stripping out suggestions from the result data
+
+    $searchResults = array_map(function($wrapper) {
+      $result = $wrapper->getRes();
+      return [
+        'url'     => $result->getUrl(),
+        'title'   => $result->getTitle(),
+        'snippet' => $result->getSnippet(),
+        'meta'    => $result->getMeta(),
+      ];
+    }, $wrappedResults);
+
+    $recommendations = array_map(function($wrapper) {
+      $recommendation = $wrapper->getRes();
+      return [
+        'url'     => $recommendation->getUrl(),
+        'title'   => $recommendation->getTitle(),
+        'snippet' => $recommendation->getSnippet(),
+        'meta'    => $recommendation->getMeta(),
+      ];
+    }, $wrappedRecommendations);
+
+    return [
+      'results'                   => $searchResults,
+      'recommendations'           => $recommendations,
+      'resOffset'                 => $resultSet->getResOffset(),
+      'resCount'                  => $resultSet->getResCount(),
+      'resStart'                  => $resultSet->getResStart(),
+      'resEnd'                    => $resultSet->getResEnd(),
+      'total'                     => $resultSet->getTotal(),
+      'literalQuery'              => $resultSet->getLiteralQuery() !== 'false',
+      'originalQueryPhrase'       => $resultSet->getOriginalQueryPhrase(),
+      'suggestionSupersededQuery' => $resultSet->getSuggestionSupersededQuery() !== 'false',
+      'supersedingSuggestion'     => $resultSet->getSupersedingSuggestion(),
+    ];
+  }
+
+  /**
+   * Filter the search $opts into a positional array to be passed to the
+   * underlying Swagger client.
+   *
+   */
+  public function getSearchParams(array $opts) : array {
+    // Translate from a boolean value to either "true" or "false".
+    // Translate anything truthy to "true".
+    $literal = ($opts['literalQuery'] ?? false) ? 'true' : 'false';
+
+    return [
+      $this->key,
+      'trees',
+      $this->collection,
+      (int) ($opts['resOffset'] ?? 0),
+      (int) ($opts['resLength'] ?? 10),
+      (string) ($opts['metaTag'] ?? ''),
+      $literal,
+    ];
+  }
+}
---
+++ test/ClientTest.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * GearLab\Api\ClientTest class
+ *
+ * @copyright 2019 SiteCrafting, Inc.
+ * @author    Coby Tamayo <ctamayo@sitecrafting.com>
+ */
+
+namespace GearLab\Api;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Tests for the GearLab\Api\Client class
+ */
+class ClientTest extends TestCase {
+  public function setUp() {
+    $this->client = new Client([
+      'key' => 'qwerty',
+      'baseUri' => 'https://fake.sitecrafting.net',
+      'collection' => 1234,
+    ]);
+  }
+
+  public function testGetSearchParams() {
+    $params = $this->client->getSearchParams([
+      'query' => 'trees',
+    ]);
+
+    $this->assertEquals([
+      'qwerty',
+      'trees',
+      1234,
+      0,
+      10,
+      '',
+      'false',
+    ], $params);
+  }
+
+  public function testGetSearchParamsResOffset() {
+    $params = $this->client->getSearchParams([
+      'query'     => 'trees',
+      'resOffset' => 30,
+    ]);
+
+    $this->assertEquals(30, $params[3]);
+  }
+
+  public function testGetSearchParamsResLength() {
+    $params = $this->client->getSearchParams([
+      'query'     => 'trees',
+      'resLength' => 17,
+    ]);
+
+    $this->assertEquals(17, $params[4]);
+  }
+
+  public function testGetSearchParamsMetaTag() {
+    $params = $this->client->getSearchParams([
+      'query'   => 'trees',
+      'metaTag' => 'abracadabra',
+    ]);
+
+    $this->assertEquals('abracadabra', $params[5]);
+  }
+
+  public function testGetSearchParamsLiteralQuery() {
+    $params = $this->client->getSearchParams([
+      'query' => 'whatever',
+      'literalQuery' => false,
+    ]);
+
+    $this->assertEquals('false', $params[6]);
+
+    $params = $this->client->getSearchParams([
+      'query' => 'whatever',
+      'literalQuery' => true,
+    ]);
+
+    $this->assertEquals('true', $params[6]);
+  }
+}
